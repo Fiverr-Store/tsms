db1ddf03e18dd603f37a11924fc2211ff35981a9
ali-dev-code <ali.asgr514@gmail.com>
2021-10-17 23:46:43 +0500

    uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+(function () {
+    //===== Prealoder
+
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
+    }
+
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+
+
+    // WOW active
+    new WOW().init();
+
+
+
+})();
